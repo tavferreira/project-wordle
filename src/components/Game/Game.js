5a5b0fcf34919c722a803f6038b32928ba01604d
Tiago Ferreira <tiagoferreira@tuta.io>
2024-06-20 17:49:09 +0200

    exercise 4 complete

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import Input from '../Input'
 import Guesses from '../Guesses'
+import { checkGuess } from '../../game-helpers';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -14,7 +15,7 @@ function Game() {
   const [guesses, setGuesses] = React.useState([])
 
   const  handleSubmitGuess = (tentativeGuess)=>  {
-    setGuesses([...guesses, tentativeGuess]);
+    setGuesses([...guesses, checkGuess(tentativeGuess,answer)]);
   }
     return (
     <>
--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -3,13 +3,12 @@ import Guess from '../Guess'
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 
-
 function Guesses({guesses}) {
   return (
     <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} value={guesses[num]} />
-      ))}
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => {
+        return <Guess key={num} value={guesses[num]} />
+      })}
     </div>
   );
 }
